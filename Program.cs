f471f0104b85c213ce6364086ad43feb80825faa
Andrey <andryuha.gonchar@mail.ru>
2024-04-11 19:11:07 +0500

    create enter_mas

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write("Введите количество строк массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] stringArray = new string[n];
+void array(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} строку массива ");
+        stringArray[i] = Console.ReadLine();
+    }
+}
